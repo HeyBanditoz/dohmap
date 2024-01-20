96d4ae5364bb312d3bedf1d92345bb7eaa86798b
Hayden <banditoz@protonmail.com>
2024-01-19 23:55:00 -0700

    Use flyway instead of home-grown migrator logic
    It'll probably cause issues later in the future

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.2.0"
     id("io.spring.dependency-management") version "1.1.4"
+    id("org.flywaydb.flyway") version "10.6.0"
 }
 
 group = "io.banditoz"
@@ -25,8 +26,15 @@ dependencies {
     implementation("com.github.f4b6a3:uuid-creator:5.3.3")
     implementation("commons-io:commons-io:2.15.1")
     developmentOnly("org.springframework.boot:spring-boot-devtools:3.2.0")
-    runtimeOnly("org.postgresql:postgresql:42.5.4")
+    runtimeOnly("org.postgresql:postgresql:42.7.1")
     testImplementation("org.springframework.boot:spring-boot-starter-test:3.2.0")
+}
+
+buildscript {
+    dependencies {
+        classpath("org.flywaydb:flyway-database-postgresql:10.6.0")
+        classpath("org.postgresql:postgresql:42.7.1")
+    }
 }
 
 tasks.withType<Test> {
--- src/main/java/io/banditoz/dohmap/database/migrator/Migration.java
+++
@@ -1,6 +0,0 @@
-package io.banditoz.dohmap.database.migrator;
-
-import java.time.OffsetDateTime;
-
-public record Migration(String filename, String executedBy, OffsetDateTime executedWhen) {
-}
