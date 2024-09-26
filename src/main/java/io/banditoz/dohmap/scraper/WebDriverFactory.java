09b8aa391fea8d02425efc9ca7622271c70852ec
Hayden <banditoz@protonmail.com>
2024-09-26 00:04:24 -0600

    Remote Selenium WebDriver support
    Set dohmap.selenium.remote to a URL

--- src/main/java/io/banditoz/dohmap/scraper/WebDriverFactory.java
+++ src/main/java/io/banditoz/dohmap/scraper/WebDriverFactory.java
@@ -4,12 +4,16 @@ import jakarta.annotation.PreDestroy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -18,20 +22,34 @@ import java.util.Set;
 public class WebDriverFactory {
     private static final Logger log = LoggerFactory.getLogger(WebDriverFactory.class);
     private final List<String> customArgs;
+    private final URL remote;
     private final Set<WebDriver> drivers = new HashSet<>();
 
     @Autowired
-    public WebDriverFactory(@Value("${dohmap.selenium.args:}") List<String> customArgs) {
+    public WebDriverFactory(@Value("${dohmap.selenium.args:}") List<String> customArgs,
+                            @Value("${dohmap.selenium.remote:}") String remote) throws MalformedURLException {
         this.customArgs = customArgs;
+        if (!(remote == null || remote.isEmpty())) {
+            this.remote = URI.create(remote).toURL();
+        } else {
+            this.remote = null;
+        }
     }
 
     public WebDriver buildWebDriver() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments(customArgs);
-        ChromeDriver driver = new ChromeDriver(options);
-        log.info("ChromeDriver {} built with custom arguments {}", driver, customArgs);
-        drivers.add(driver);
-        return driver;
+        if (remote == null) {
+            ChromeDriver driver = new ChromeDriver(options);
+            log.info("ChromeDriver {} built with custom arguments {}", driver, customArgs);
+            drivers.add(driver);
+            return driver;
+        } else {
+            WebDriver driver = new RemoteWebDriver(remote, options);
+            log.info("RemoteWebDriver {} built with custom arguments {}", driver, customArgs);
+            drivers.add(driver);
+            return driver;
+        }
     }
 
     public void disposeDriver(WebDriver driver) {
